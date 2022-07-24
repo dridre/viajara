ae75026738646ad8eaf76da82c3082ece8287f8e
dridre <rubenborque@hotmail.com>
2022-07-25 00:48:08 +0200

    Signed-off-by: dridre <rubenborque@hotmail.com>

--- src/components/CocheIndividual.jsx
+++ src/components/CocheIndividual.jsx
@@ -1,19 +1,9 @@
 import React, { useEffect, useState } from "react";
 
-const CocheIndividual = () => {
-  const [isEditing, setEditing] = useState(true);
-  const handleKeyDown = (event, type) => {
-    // Handle when key is pressed
-  };
-  return (
-    <div>
-      {isEditing ? (
-        <div onClick={() => setEditing(false)}>adios</div>
-      ) : (
-        <input />
-      )}
-    </div>
-  );
+const CocheIndividual = (id) => {
+  console.log(id);
+
+  return <div>hola</div>;
 };
 
 export default CocheIndividual;
